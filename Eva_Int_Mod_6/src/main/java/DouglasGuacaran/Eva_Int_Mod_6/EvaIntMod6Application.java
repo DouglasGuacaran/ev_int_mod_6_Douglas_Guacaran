aecb2b3c4e8017b6cdd4c34a13df41c9773c1362
DouglasGuacaran <douglasguacaran@gmail.com>
2024-06-20 10:15:19 -0400

    Primer commit

---
+++ Eva_Int_Mod_6/src/main/java/DouglasGuacaran/Eva_Int_Mod_6/EvaIntMod6Application.java
@@ -0,0 +1,13 @@
+package DouglasGuacaran.Eva_Int_Mod_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaIntMod6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaIntMod6Application.class, args);
+	}
+
+}
---
+++ Eva_Int_Mod_6/src/test/java/DouglasGuacaran/Eva_Int_Mod_6/EvaIntMod6ApplicationTests.java
@@ -0,0 +1,13 @@
+package DouglasGuacaran.Eva_Int_Mod_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaIntMod6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
